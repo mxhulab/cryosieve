c68d6d6d59b46df9b84632762d35eb1d2da72b81
zerinf <2252749718@qq.com>
2024-08-03 20:15:29 +0800

    update v1.2.7

--- src/cryosieve/cs_refine.py
+++ src/cryosieve/cs_refine.py
@@ -25,6 +25,8 @@ def parse_argument():
     parser.add_argument('--workspace', type = str, required = True, help = 'workspace UID in CryoSPARC.')
     parser.add_argument('--lane',      type = str, required = True, help = 'lane selected for computing in CryoSPARC.')
     parser.add_argument('--nu',        action = 'store_true',       help = 'use non-uniform refinement.')
+    parser.add_argument('--local',     action = 'store_true',       help = 'use local refinement after homogeneous / non-uniform refinement.')
+    parser.add_argument('--min_angular_step', type = float, default = 0.01, help = 'minimum angular step for local refinement.')
     parser.add_argument('--resplit',   action = 'store_true',       help = 'force re-do GS split.')
     parser.add_argument('--workers',   type = int,                  help = 'number of workers to run CryoSPARC job, unlimited by default.')
     if len(sys.argv) == 1:
@@ -56,46 +58,69 @@ def enqueue_and_wait(local_lane, **kwargs):
         client.enqueue_job(project_uid, job_id, local_lane, user_id)
 
     while True:
-        sleep(uniform(30, 60))
+        sleep(uniform(20, 40))
         with lock: job_status = client.get_job_status(project_uid, job_id)
         if job_status == 'completed':
             return job_id
         elif job_status in ['failed', 'killed']:
             raise RuntimeError(f'Job {job_id} is {job_status}.')
 
-def refine(particle_meta_path, particle_blob_dir):
+def import_particles(particle_meta_path):
     import_job_id = enqueue_and_wait(
         None,
         job_type = 'import_particles',
         params = {
             'particle_meta_path' : particle_meta_path,
-            'particle_blob_path' : particle_blob_dir
+            'particle_blob_path' : str(Path(args.directory).absolute())
         }
     )
+    return import_job_id
 
-    if not import_ref_job_id:
-        abinit_job_id = enqueue_and_wait(
-            lane,
-            job_type = 'homo_abinit',
-            input_group_connects = {
-                'particles': f'{import_job_id}.imported_particles'
-            }
-        )
+def abinit(import_particles_job_id):
+    abinit_job_id = enqueue_and_wait(
+        lane,
+        job_type = 'homo_abinit',
+        input_group_connects = {
+            'particles': f'{import_particles_job_id}.imported_particles'
+        }
+    )
+    return abinit_job_id
 
-    params = {}
-    params['refine_symmetry'] = args.sym
-    params['refine_gs_resplit'] = args.resplit
+def refine(particle, volume):
+    params = {
+        'refine_symmetry' : args.sym,
+        'refine_gs_resplit' : args.resplit
+    }
     if args.ini_high is not None: params['refine_res_init'] = args.ini_high
     refine_job_id = enqueue_and_wait(
         lane,
         job_type = 'nonuniform_refine_new' if args.nu else 'homo_refine_new',
         params = params,
         input_group_connects = {
-            'particles' : f'{import_job_id}.imported_particles',
-            'volume' : f'{import_ref_job_id}.imported_volume_1' if import_ref_job_id else f'{abinit_job_id}.volume_class_0'
+            'particles' : particle,
+            'volume' : volume
         }
     )
+    return refine_job_id
 
+def local_refine(refine_job_id):
+    local_refine_job_id = enqueue_and_wait(
+        lane,
+        job_type = 'new_local_refine',
+        params = {
+            'refine_symmetry' : args.sym,
+            'use_alignment_prior' : True,
+            'min_angular_step' : args.min_angular_step
+        },
+        input_group_connects = {
+            'particles' : f'{refine_job_id}.particles',
+            'volume' : f'{refine_job_id}.volume',
+            'mask' : f'{refine_job_id}.mask'
+        }
+    )
+    return local_refine_job_id
+
+def parse_result(refine_job_id):
     with lock: streamlog = client.get_job_streamlog(project_uid, refine_job_id)
     text_list = [entry['text'].strip() for entry in streamlog if 'text' in entry]
     resolution = bfactor = None
@@ -108,7 +133,15 @@ def refine(particle_meta_path, particle_blob_dir):
             num_A = int(re.search(r'\b(\d+)\b', text).group(1))
         elif text.startswith('Split B has'):
             num_B = int(re.search(r'\b(\d+)\b', text).group(1))
-    return [particle_meta_path, num_A + num_B, resolution, bfactor]
+
+    with lock: alignment3D_path = client.get_project_dir_abs(project_uid) + '/' + client.get_job_result(project_uid, f'{refine_job_id}.particles.alignments3D')['metafile']
+    return [num_A + num_B, resolution, bfactor, alignment3D_path]
+
+def check_results(results, info = None):
+    if info is not None:
+        print(info, results)
+    if any(result is None for result in results):
+        raise RuntimeError('Error occurred during execution.')
 
 def main():
     args = parse_argument()
@@ -126,6 +159,7 @@ def main():
               f'--workspace {args.workspace} ' + \
               f'--lane {args.lane} ' + \
               ('--nu ' if args.nu else '') + \
+              ('--local ' if args.local else '') + \
               ('--resplit ' if args.resplit else '') + \
               (f'--workers {args.workers} ' if args.workers is not None else '')
     subprocess.run(command, shell = True)
@@ -155,14 +189,21 @@ if __name__ == '__main__':
     particle_meta_paths = parse_meta_paths(args.i)
     if not Path(args.directory).is_dir():
         raise FileNotFoundError(f'"{args.directory}" is not a directory.')
-    particle_blob_dir = str(Path(args.directory).absolute())
-    csvfile = open(args.o, 'w')
-    csvwriter = csv.writer(csvfile)
-
-    # If ref is given, import it.
     if args.ref is not None:
         if not Path(args.ref).is_file():
-            raise FileNotFoundError(f'"{args.ref}" not exists.')
+            raise FileNotFoundError(f'"{args.ref}" does not exist.')
+
+    # Setup.
+    pool = ThreadPoolExecutor() if args.workers is None else ThreadPoolExecutor(max_workers = args.workers)
+
+    # Import particles.
+    import_particles_tasks = [pool.submit(import_particles, particle_meta_path) for particle_meta_path in particle_meta_paths]
+    import_particles_job_ids = [task.result() for task in import_particles_tasks]
+    check_results(import_particles_job_ids, 'Import particles:')
+    particles = [f'{import_particles_job_id}.imported_particles' for import_particles_job_id in import_particles_job_ids]
+
+    # Import volumes, if ref is given.
+    if args.ref is not None:
         import_ref_job_id = enqueue_and_wait(
             None,
             job_type = 'import_volumes',
@@ -170,18 +211,38 @@ if __name__ == '__main__':
                 'volume_blob_path' : str(Path(args.ref).absolute())
             }
         )
+        check_results([import_ref_job_id], 'Imported reference maps:')
+        volumes = [f'{import_ref_job_id}.imported_volume_1'] * len(particles)
+    # Otherwise, use ab-initio reconstruction.
     else:
-        import_ref_job_id = ''
+        abinit_tasks = [pool.submit(abinit, import_particles_job_id) for import_particles_job_id in import_particles_job_ids]
+        abinit_job_ids = [task.result() for task in abinit_tasks]
+        check_results(abinit_job_ids, 'Ab-initio reconstruction:')
+        volumes = [f'{abinit_job_id}.volume_class_0' for abinit_job_id in abinit_job_ids]
 
-    # Call CryoSPARC's refinement.
-    pool = ThreadPoolExecutor() if args.workers is None else ThreadPoolExecutor(max_workers = args.workers)
-    futures = [pool.submit(refine, particle_meta_path, particle_blob_dir) for particle_meta_path, _ in product(particle_meta_paths, range(args.repeat))]
-    results = [future.result() for future in futures]
+    # Homogenous / non-uniform refinement.
+    refine_tasks = [pool.submit(refine, particle, volume) for (particle, volume), _ in product(zip(particles, volumes), range(args.repeat))]
+    refine_job_ids = [task.result() for task in refine_tasks]
+    check_results(refine_job_ids, 'Homogeneous / Non-uniform refinement:')
+
+    # Local refinement.
+    if args.local:
+        refine_tasks = [pool.submit(local_refine, refine_job_id) for refine_job_id in refine_job_ids]
+        refine_job_ids = [task.result() for task in refine_tasks]
+        check_results(refine_job_ids, 'Local refinement:')
+
+    # Get results.
+    parse_result_tasks = [pool.submit(parse_result, refine_job_id) for refine_job_id in refine_job_ids]
+    results = [task.result() for task in parse_result_tasks]
+    check_results(results)
+
+    # Shutdown.
     pool.shutdown()
-    if any(result is None for result in results):
-        raise RuntimeError('Error occurred during CryoSPARC refinement.')
 
     # Save results.
-    csvwriter.writerow(['filename', 'number of particles', 'resolution (A)', 'b-factor (A^2)'])
-    for result in results: csvwriter.writerow(result)
-    csvfile.close()
+    with open(args.o, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['filename', 'number of particles', 'resolution (A)', 'b-factor (A^2)', 'alignment3D'])
+        for i, result in enumerate(results):
+            particle_meta_path = particle_meta_paths[i // args.repeat]
+            csvwriter.writerow([particle_meta_path] + result)
